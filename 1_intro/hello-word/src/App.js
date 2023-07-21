f1a519927ee7c2fe912fe4b805577197f61325e8
Moises Sutil <131698525+moisessutil@users.noreply.github.com>
2023-07-21 18:03:35 -0300

    fundamentos

---
+++ 1_intro/hello-word/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Word</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam harum corporis ducimus sunt maiores quis, tempore veritatis quos facere, eveniet eius voluptate cumque fugit corrupti rem id reprehenderit similique asperiores.</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ 2_fundamentos/fundamentos/src/App.js
@@ -0,0 +1,18 @@
+// componests
+import FristComponent from './components/FristComponent';
+
+
+
+// style / css
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fundamentos do React</h1>
+      <FristComponent />
+    </div>
+  );
+}
+
+export default App;
