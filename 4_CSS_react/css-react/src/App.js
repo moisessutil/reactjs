c70389a8de390a2128bcaa76af3f66159d45ac6e
Moises Sutil <131698525+moisessutil@users.noreply.github.com>
2023-08-09 20:45:30 -0300

    novo ex

--- 4_CSS_react/css-react/src/App.js
+++ 4_CSS_react/css-react/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import MyComponent from './components/MyComponent';
+import useState from 'react';
 
 function App() {
+  const n = 15
+  {/*const [name] = useState('Pedro')*/}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* css global */}
+      <h1>React com CSS</h1>
+
+      {/* css componente */}
+      <MyComponent />
+      <p>Esse paragrafo é do App.js</p>
+
+      {/* inline style */}
+      <p style={{ 
+        color: 'blue', 
+        padding: '10px', 
+        borderTop: 'solid 3px red' }}>
+          Esse elemento está sendo estilizado inline
+          </p>
+
+      {/* css inline dinamico */}
+      <h2 style={n < 10 ? ({color: 'purple'}) : ({color: 'pink'})}>CSS dinâmico</h2>
+      <h2 style={n > 10 ? ({color: 'purple'}) : ({color: 'pink'})}>CSS dinâmico</h2>
+      {/*<h2 style={name === 'Pedro' ? ({color: 'purple'}) : ({color: 'pink'})}>CSS dinâmico</h2>*/}
     </div>
   );
 }
---
+++ 4_CSS_react/css-react/src/components/MyComponent.js
@@ -0,0 +1,13 @@
+import "./MyComponent.css"
+
+const MyComponent = () => {
+  return (
+    <div>
+        <h1>CSS com Componente</h1>
+        <p>Esse é paragráfo do componente</p>
+        <p className='my-comp' >Esse também é componente</p>
+    </div>
+  )
+}
+
+export default MyComponent
